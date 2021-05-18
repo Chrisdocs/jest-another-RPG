01e04ce7bdf35b42f9fc9c8808af5c08996b0090
Chris Wolfe <chrisa.wolfe@gmail.com>
2021-05-18 16:17:28 -0500

    prototype contructors

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,7 +1,6 @@
 const Potion = require('../lib/Potion');
 
 jest.mock('../lib/Potion');
-console.log(new Potion());
 
 const Player = require('../lib/Player');
 
@@ -13,4 +12,23 @@ test('Creates a player object', () => {
     expect(player.strength).toEqual(expect.any(Number));
     expect(player.agility).toEqual(expect.any(Number));
     expect(player.inventory).toEqual(expect.arrayContaining([expect.any(Object)]));
+});
+
+test('Gets players stats as an object', () => {
+    const player = new Player('Dave');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('Gets inventory from player ot returns false', () => {
+    const player = new Player('Dave');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
 });
